6d1d405d4589b250367281793d318a3666f4861c
Jacob198282 <s198282@student.pg.edu.pl>
2025-12-03 20:26:25 +0100

    Main menu as User Control

---
+++ MainMenuControl.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace bezpieczna_paczkaApp
+{
+    partial class MainMenuControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            picLogo = new PictureBox();
+            picSelectLevel = new PictureBox();
+            picExitGame = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)picLogo).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)picSelectLevel).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)picExitGame).BeginInit();
+            SuspendLayout();
+            // 
+            // picLogo
+            // 
+            picLogo.BackColor = Color.Transparent;
+            picLogo.Location = new Point(440, 100);
+            picLogo.Margin = new Padding(4, 3, 4, 3);
+            picLogo.Name = "picLogo";
+            picLogo.Size = new Size(400, 400);
+            picLogo.SizeMode = PictureBoxSizeMode.StretchImage;
+            picLogo.TabIndex = 0;
+            picLogo.TabStop = false;
+            // 
+            // picSelectLevel
+            // 
+            picSelectLevel.BackColor = Color.Transparent;
+            picSelectLevel.Cursor = Cursors.Hand;
+            picSelectLevel.Location = new Point(174, 577);
+            picSelectLevel.Margin = new Padding(4, 3, 4, 3);
+            picSelectLevel.Name = "picSelectLevel";
+            picSelectLevel.Size = new Size(290, 115);
+            picSelectLevel.SizeMode = PictureBoxSizeMode.Zoom;
+            picSelectLevel.TabIndex = 1;
+            picSelectLevel.TabStop = false;
+            picSelectLevel.Click += picSelectLevel_Click;
+            // 
+            // picExitGame
+            // 
+            picExitGame.BackColor = Color.Transparent;
+            picExitGame.Cursor = Cursors.Hand;
+            picExitGame.Location = new Point(790, 577);
+            picExitGame.Margin = new Padding(4, 3, 4, 3);
+            picExitGame.Name = "picExitGame";
+            picExitGame.Size = new Size(292, 115);
+            picExitGame.SizeMode = PictureBoxSizeMode.Zoom;
+            picExitGame.TabIndex = 2;
+            picExitGame.TabStop = false;
+            picExitGame.Click += picExitGame_Click;
+            // 
+            // MainMenuControl
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Transparent;
+            Controls.Add(picExitGame);
+            Controls.Add(picSelectLevel);
+            Controls.Add(picLogo);
+            DoubleBuffered = true;
+            Name = "MainMenuControl";
+            Size = new Size(1280, 1024);
+            Load += MainMenuConrol_Load;
+            ((System.ComponentModel.ISupportInitialize)picLogo).EndInit();
+            ((System.ComponentModel.ISupportInitialize)picSelectLevel).EndInit();
+            ((System.ComponentModel.ISupportInitialize)picExitGame).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private PictureBox picLogo;
+        private PictureBox picSelectLevel;
+        private PictureBox picExitGame;
+    }
+
+}
---
+++ MainMenuControl.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace bezpieczna_paczkaApp
+{
+    // UserControl for the main menu screen.
+    public partial class MainMenuControl : UserControl
+    {
+        // Public event to notify the host form (GameWindow) when the level selection button is clicked.
+        public event EventHandler SelectLevelClicked;
+
+        public MainMenuControl()
+        {
+            InitializeComponent();
+            
+            DoubleBuffered = true; // removes flickering of the background
+        }
+
+        // Handles the Click event for the 'Select Level' button.
+        private void picSelectLevel_Click(object sender, EventArgs e)
+        {
+            // Notify the GameWindow to switch screens.
+            SelectLevelClicked?.Invoke(this, EventArgs.Empty);
+        }
+
+        // Handles the Click event for the 'Exit Game' button.
+        private void picExitGame_Click(object sender, EventArgs e)
+        {
+            // Safely exits the application.
+            Application.Exit();
+        }
+        private void MainMenuConrol_Load(object sender, EventArgs e)
+        {
+            // You can add any initialization code here that needs to run
+            // after the components are loaded.
+            // For example, loading high scores, user settings, etc.
+            // This is the best place to load images from files.
+            LoadGraphics();
+        }
+
+
+        /// Loads all graphical assets from the 'graphics' folder.
+
+        private void LoadGraphics()
+        {
+            // Get the directory where the .exe file is running
+            string basePath = Application.StartupPath;
+            string graphicsPath = Path.Combine(basePath, "graphics");
+
+            try
+            {
+                // Load the image for the Select Level button
+                string selectLevelPath = Path.Combine(graphicsPath, "wybierz-poziom.png");
+                picSelectLevel.Image = Image.FromFile(selectLevelPath);
+
+                // Load the image for the logo
+                string logoPath = Path.Combine(graphicsPath, "bezpieczna-paczka-logo-nobg.png");
+                picLogo.Image = Image.FromFile(logoPath);
+
+                // Load the image for the Exit Button
+                string exitPath = Path.Combine(graphicsPath, "wyjdz-z-gry.png");
+                picExitGame.Image = Image.FromFile(exitPath);
+            }
+            catch (FileNotFoundException ex)
+            {
+                // If a file is missing, show a helpful error message instead of crashing.
+                MessageBox.Show($"Błąd wczytywania grafiki: Nie znaleziono pliku!\n{ex.Message}", "Błąd Pliku");
+                Application.Exit(); // Exit if graphics are missing
+            }
+            catch (Exception ex)
+            {
+                // Catch other potential errors (e.g., file is not an image)
+                MessageBox.Show($"Wystąpił nieoczekiwany błąd przy wczytywaniu grafiki:\n{ex.Message}", "Błąd Krytyczny");
+                Application.Exit();
+            }
+        }
+    }
+}
