348faafc118afc7b732bdc24485f7d6d954a8e6b
Jacob198282 <s198282@student.pg.edu.pl>
2026-01-05 21:11:30 +0100

    In-game menu UserControl

---
+++ GameMenuControl.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bezpieczna_paczkaApp
+{
+    public partial class GameMenuControl : UserControl
+    {
+        // Events to notify the parent container about user choices
+        public event EventHandler ResumeClicked;
+        public event EventHandler ExitToLevelSelectClicked;
+        public GameMenuControl()
+        {
+            // Initialize component
+            InitializeComponent();
+            DoubleBuffered = true; // anti-flickering 
+            // Load graphics fot the in-game menu
+            Dock = DockStyle.Fill;
+            LoadGraphics();
+        }
+        private void picResume_Click(object sender, EventArgs e)
+        {
+            // Fire the event to close the menu
+            ResumeClicked?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void picSelectLevel_Click(object sender, EventArgs e)
+        {
+            // Fire the event to go back to level selection
+            ExitToLevelSelectClicked?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void picExit_Click(object sender, EventArgs e)
+        {
+            // Close the entire application
+            Application.Exit();
+        }
+
+
+        private void LoadGraphics()
+        {
+            string basePath = Application.StartupPath;
+            string graphicsPath = Path.Combine(basePath, "graphics");
+
+            try
+            {
+                // Load image for the back button
+                string menuButtonPath = Path.Combine(graphicsPath, "powrot.png");
+                picResume.Image = Image.FromFile(menuButtonPath);
+
+                // Load image for level select menu button
+                string logoPath = Path.Combine(graphicsPath, "wybierz-poziom.png");
+                picLevelSelect.Image = Image.FromFile(logoPath);
+
+                // Load image for the exit button
+                string uniPath = Path.Combine(graphicsPath, "wyjdz-z-gry.png");
+                picExit.Image = Image.FromFile(uniPath);
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show(
+                    $"Błąd wczytywania grafiki przycisku menu: Nie znaleziono pliku!\n{ex.Message}",
+                    "Błąd Pliku");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    $"Wystąpił nieoczekiwany błąd przy wczytywaniu grafiki przycisku menu:\n{ex.Message}",
+                    "Błąd Krytyczny");
+            }
+        }
+    }
+}
--- README.md
+++
@@ -1,9 +0,0 @@
-# Bezpieczna Paczka - driving rules learning game
-## What is it?
-"Bezpieczna paczka" is game that lets the user learn or remind driving rules that apply in Poland while delivering packages to different destinations in game.
-## Contents of this repository
-`TODO`
-## How to run the game?
-`TODO`
-## How to play the game?
-`TODO`
