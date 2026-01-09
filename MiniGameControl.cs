3cfc36e31c68cd703b7fab79315f40266fde2ae8
Jacob198282 <s198282@student.pg.edu.pl>
2026-01-09 18:57:07 +0100

    Started creation of new minigame before questions

---
+++ MiniGameControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bezpieczna_paczkaApp.src
+{
+    public partial class MiniGameControl : UserControl
+    {
+        public MiniGameControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Package.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bezpieczna_paczkaApp.src
+{
+    internal class Package
+    {
+    }
+}
